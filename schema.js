9047ebf5579cfd8aef7aee38e6342d3989610783
HoWWWWWhy <HoWWWWWhy@gmail.com>
2021-04-18 21:50:31 +0900

    #3.10 Architecture part Two - File architecture divide and conquer

--- schema.js
+++ schema.js
@@ -1,2 +1,12 @@
+import { loadFilesSync, mergeTypeDefs, mergeResolvers, makeExecutableSchema } from "graphql-tools";
 
+// loadFilesSync는 export default를 가져옴
+const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.js`);
+const loadedResolvers = loadFilesSync(`${__dirname}/**/*.{queries,mutation}.js`);
 
+const typeDefs = mergeTypeDefs(loadedTypes);
+const resolvers = mergeResolvers(loadedResolvers);
+
+const schema = makeExecutableSchema({typeDefs, resolvers})
+
+export default schema;
