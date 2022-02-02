712efeb9037aa17664143fc266a4e23b9a948c56
HoWWWWWhy <HoWWWWWhy@gmail.com>
2022-02-03 00:07:39 +0900

    #4.4-4.5 login and refactor

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -1,5 +1,6 @@
 import client from "../client";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export default {
   Mutation: {
@@ -43,6 +44,34 @@ export default {
       } catch (error) {
         return error;
       }
+    },
+    login: async (_, { username, password }) => {
+      // find user with args.username
+      const user = await client.user.findFirst({
+        where: { username },
+      });
+      if (!user) {
+        return {
+          ok: false,
+          error: "User not found.",
+        };
+      }
+      // check password with args.password
+      const passwordOK = await bcrypt.compare(password, user.password);
+      console.log("password OK?", passwordOK);
+      if (!passwordOK) {
+        return {
+          ok: false,
+          error: "Incorrect password.",
+        };
+      }
+      // issue a token and send it to the user
+      // token: jsonwebtoken
+      const token = await jwt.sign({ id: user.id }, process.env.SECRET_KEY);
+      return {
+        ok: true,
+        token,
+      };
     },
   },
 };
