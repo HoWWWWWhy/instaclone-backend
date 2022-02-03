b570974f82e9e1fe9f3191a080031b3c030d4585
HoWWWWWhy <HoWWWWWhy@gmail.com>
2022-02-03 16:59:27 +0900

    #4.7 updateProfile

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,7 +1,43 @@
+import client from "../../client";
+import bcrypt from "bcrypt";
+
 export default {
   Mutation: {
-    editProfile: () => {
+    editProfile: async (
+      _,
+      { firstName, lastName, username, email, password: newPassword }
+    ) => {
       console.log("editProfile");
+      console.log(firstName, lastName, username, email, newPassword);
+
+      // hash password
+      let uglyPassword = null;
+      if (newPassword) {
+        const saltRounds = 10;
+        uglyPassword = await bcrypt.hash(newPassword, saltRounds);
+      }
+
+      const updatedUser = await client.user.update({
+        where: { id: 1 },
+        data: {
+          firstName,
+          lastName,
+          username,
+          email,
+          ...(uglyPassword && { password: uglyPassword }),
+        },
+      });
+      console.log("updatedUser", updatedUser);
+      if (updatedUser.id) {
+        return {
+          ok: true,
+        };
+      } else {
+        return {
+          ok: false,
+          error: "Could not update profile.",
+        };
+      }
     },
   },
 };
